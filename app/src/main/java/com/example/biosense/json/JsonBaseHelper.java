57a0173f6b9479bb184b67e25f2ca62069229bcb
Pedro Victor Eugênio <pvesouza@gmail.com>
2023-12-20 17:14:09 -0300

    Json saved file

--- app/src/main/java/com/example/biosense/json/JsonBaseHelper.java
+++ app/src/main/java/com/example/biosense/json/JsonBaseHelper.java
@@ -2,11 +2,20 @@ package com.example.biosense.json;
 
 // This class contains the methods to create a JSON file and save it as a csv or a JSON
 
+import android.content.Context;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
 
 public class JsonBaseHelper {
     private File file;
@@ -18,7 +27,7 @@ public class JsonBaseHelper {
     private String filename;
     private String filepath;
 
-    private String jsonString;
+    private static final String TAG = "JsonBaseHelper";
 
     public JsonBaseHelper(String filepath, String filename) {
         this.filename = filename;
@@ -29,9 +38,42 @@ public class JsonBaseHelper {
 
     }
 
-    public void addJsonLine(String json) {
-        this.jsonString += json;
+    public void saveJson(Context ctx, String data) throws JsonSaveException {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("Data", data);
+        } catch (JSONException e) {
+            throw new JsonSaveException("Error putting data into Json");
+        }
+        File file = new File(ctx.getFilesDir(), Date.from(Instant.now()) + ".json");
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(file);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(jsonObject.toString());
+            bufferedWriter.close();
+            Log.d(TAG, "saveJson: " + ctx.getFilesDir() + Date.from(Instant.now()) + ".json");
+        } catch (IOException e) {
+            throw new JsonSaveException("Error to write Json File");
+        }
+
     }
-
-
+    public void saveJson(String data) throws JsonSaveException {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("Data", data);
+        } catch (JSONException e) {
+            throw new JsonSaveException("Error putting data into Json");
+        }
+        File file = new File(this.filepath, this.filename + ".json");
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(file);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(jsonObject.toString());
+            bufferedWriter.close();
+        } catch (IOException e) {
+            throw new JsonSaveException("Error to write Json File");
+        }
+    }
 }
