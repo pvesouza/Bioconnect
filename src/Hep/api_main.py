eabf269e391e3c5da52bc8255b8b23835df5a28a
Pedro Victor Eugênio <pvesouza@gmail.com>
2023-12-31 17:46:30 -0300

    API integrated with smartphone and working on local IP

--- src/Hep/api_main.py
+++ src/Hep/api_main.py
@@ -1,6 +1,7 @@
 from flask import Flask, make_response, jsonify, request
 from inference_hep import Hep
 from inference_rhod import Rhod
+from scipy.signal  import resample
 import numpy as np
 
 app = Flask("hepatitis")
@@ -12,8 +13,10 @@ def test_api():
 
 @app.route("/hep", methods=['POST'])
 def analyse_hep():
+    
     try:
         json = request.json
+        #print(json)
         json_readings = json["readings"]
         potential = []
         current = []
@@ -24,11 +27,12 @@ def analyse_hep():
             c = np.float32(sample["I"])
             potential.append(v)
             current.append(c)
+            #print(v,c)
         
         hep_analysis = Hep()
         result = hep_analysis.predict_data(np.array(current))
         
-        print(result)
+        #print(result)
         return make_response(jsonify(result), 200)
     
     except:
@@ -40,8 +44,10 @@ def analyse_rhod():
         
         json = request.json
         json_readings = json["readings"]
+        #print(len(json_readings))
         potential = []
         current = []
+        inference_module = Rhod("C:\\Users\\pveso\\Documents\\heart_attack_analysis\\src\\Hep\\Models\\Rhod\\cnn_rod.h5")
         
         for i in range(len(json_readings)):
             sample = json_readings[i]
@@ -49,10 +55,27 @@ def analyse_rhod():
             c = float(sample["I"]) 
             potential.append(v)
             current.append(c)
+            #print(v,c)
+        
+        # 1115 is the shape that cnn was trained
+        if (len(current) < 1115):
             
-        inference_module = Rhod("C:\\Users\\pveso\\Documents\\heart_attack_analysis\\src\\Hep\\Models\\Rhod\\cnn_rod.h5")
-        data = np.array(current).reshape(-1, 1)
-        result = inference_module.predict_sample(data)
+            return make_response(jsonify("Less Samples"), 500)
+        elif (len(current) == 1115):           
+           
+            data = np.array(current).reshape(-1, 1)
+            #print(data.shape)
+            result = inference_module.predict_sample(data)
+        else:
+   
+            data = np.array(current).reshape(-1, 1)
+            #print(f"Old Shape: {data.shape}")
+            diff = abs(data.shape[0] - 1115)
+            n_of_samples = data.shape[0] - diff
+            data =  resample(data, n_of_samples, domain='time')
+            #print(f"New Shape: {data.shape}")
+            
+            result = inference_module.predict_sample(data) 
         
         return make_response(jsonify(result), 200)  
     except:
@@ -60,4 +83,4 @@ def analyse_rhod():
         return make_response(jsonify("Error"), 500)
     
 #Start Api
-app.run()
+app.run(host="192.168.0.122")
--- src/Hep/inference_rhod.py
+++ src/Hep/inference_rhod.py
@@ -1,7 +1,5 @@
 import numpy as np
-import joblib
 import tensorflow as tf
-import os
 
 class Rhod():
     
@@ -9,20 +7,37 @@ class Rhod():
         self.model = tf.keras.models.load_model(file)
         
     def normalize(self, reshaped_sample):
+        #print("Normalization")
         # normalization min-max
+        if (len (reshaped_sample) == 0):
+            return None
         copy = reshaped_sample.copy()
         max_ = np.max(copy)
         min_ = np.min(copy)
+        print(max_, min_)
         copy[:,0] = ((copy[:,0] - min_) / (max_ - min_))
+        #print(np.max( copy[:,0]), np.min( copy[:,0]))
         return copy
     
     def predict_sample(self, sample_data):
-        normalized_data = self.normalize(sample_data)
-        # Reshape normalized data
-        normalized_data = normalized_data.reshape(1, -1, 1)
-        result = self.model.predict(normalized_data)
         
-        if result > 0.5:
-            return 1
+        #print("Predict")
+        if (len(sample_data) == 0):
+            return 0
+        
+        normalized_data = self.normalize(sample_data)
+        #print("Pos Normal")
+        
+        if (normalized_data.shape[0] > 0):
+            # Reshape normalized data
+            #print(normalized_data.shape)
+            normalized_data = normalized_data.reshape(1, -1, 1)
+            result = self.model.predict(normalized_data)
+            
+            if result > 0.5:
+                return 1
+            else:
+                return 0
         else:
-            return 0      
+            print("Erro na normalização")
+        return 0
